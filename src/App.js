a602c4727bfd93afa0154b73016a6cc6a911fe75
selsadan <danielfrancis442@gmail.com>
2023-07-31 12:02:25 +0100

    React state over time

--- src/App.js
+++ src/App.js
@@ -2642,19 +2642,36 @@
 // }
 
 
+// import { useState } from "react";
+
+// export default function App(){
+//   const [number, setNumber] = useState(0);
+
+//   return(
+//     <>
+//       <h1>{number}</h1>
+//       <button onClick={() => {
+//         setNumber(number + 1);
+//         setNumber(number + 1);
+//         setNumber(number + 1);
+//       }}>+3</button>
+//     </>
+//   );
+// }
+
+// React State over time
 import { useState } from "react";
 
-export default function App(){
-  const [number, setNumber] = useState(0);
+export default function Counter(){
+    const [number, setNumber] = useState(0);
 
-  return(
-    <>
-      <h1>{number}</h1>
-      <button onClick={() => {
-        setNumber(number + 1);
-        setNumber(number + 1);
-        setNumber(number + 1);
-      }}>+3</button>
-    </>
-  );
+    return(
+        <>
+            <h1>{number}</h1>
+            <button onClick={() => {
+                setNumber(number + 5);
+                alert(number);
+            }}>+5</button>
+        </>
+    );
 }
