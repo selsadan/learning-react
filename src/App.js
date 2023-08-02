2b326ffd49663e40edc9ef59b67a691b156c79ce
selsadan <danielfrancis442@gmail.com>
2023-08-02 12:20:53 +0100

    learning what happen if you update state after replacing it in React seconf time

--- src/App.js
+++ src/App.js
@@ -2985,6 +2985,22 @@
 
 // What happen if you update state after replacing it in React
 
+// import { useState } from "react";
+
+// export default function App(){
+//     const [number, setNumber] = useState(0);
+
+//     return(
+//         <>
+//             <h1>{number}</h1>
+//             <button onClick={() => {
+//                 setNumber(number + 5);
+//                 setNumber(n => n + 1);
+//             }}>Increase the number</button>
+//         </>
+//     );
+// }
+
 import { useState } from "react";
 
 export default function App(){
@@ -2994,9 +3010,10 @@ export default function App(){
         <>
             <h1>{number}</h1>
             <button onClick={() => {
-                setNumber(number + 5);
+                setNumber(number + 1);
                 setNumber(n => n + 1);
-            }}>Increase the number</button>
+                setNumber(42);
+            }}>Increase Number</button>
         </>
     );
 }
