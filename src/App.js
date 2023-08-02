a87d2c2af6cc8089fa10f88d7e289fafc413e787
selsadan <danielfrancis442@gmail.com>
2023-08-02 10:06:59 +0100

    re-learning React state as snapshot for the third time

--- src/App.js
+++ src/App.js
@@ -2867,20 +2867,57 @@
 //     );
 // }
 
+// import { useState } from "react";
+
+// export default function App(){
+//     const [number, setNumber] = useState(0);
+
+//     return(
+//         <>
+//             <h1>{number}</h1>
+//             <button onClick={() => {
+//                 setNumber(number + 1);
+//                 setTimeout(() => {
+//                     alert(number);
+//                 }, 3000);
+//             }}>+5</button>
+//         </>
+//     );
+// }
+
 import { useState } from "react";
 
 export default function App(){
-    const [number, setNumber] = useState(0);
+    const [to, setTo] = useState('Alice');
+    const [message, setMessage] = useState('Hello');
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setTimeout(() => {
+            alert(`You said ${message} to ${to}`)
+        }, 5000);
+    }
 
     return(
-        <>
-            <h1>{number}</h1>
-            <button onClick={() => {
-                setNumber(number + 1);
-                setTimeout(() => {
-                    alert(number);
-                }, 3000);
-            }}>+5</button>
-        </>
+        <form onSubmit={handleSubmit}>
+            <label>
+                To: {''}
+                <select 
+                    value={to}
+                    onChange={e => setTo(e.target.value)}
+                >
+                    <option value='Alice'>Alice</option>
+                    <option value='Bob'>Bob</option>
+                </select>
+            </label>
+            <br />
+            <textarea 
+                placeholder="Message"
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+            />
+            <br />
+            <button type="submit">Send</button>
+        </form>
     );
 }
