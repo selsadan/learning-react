bea61632b78805fe5ddfee5663e3c0ad07792650
selsadan <danielfrancis442@gmail.com>
2023-08-11 09:41:41 +0100

    learning React object spread syntax

--- src/App.js
+++ src/App.js
@@ -3373,38 +3373,94 @@
 
 
 
+// import { useState } from "react";
+
+// export default function MovingDot(){
+//     const [position, setPosition] = useState({
+//         x: 0,
+//         y: 0,
+//     });
+
+//     return(
+//         <div onPointerMove={e => {
+//             setPosition({
+//                 x: e.clientX,
+//                 y: e.clientY,
+//             });
+//         }}
+//         style={{
+//         position: 'relative',
+//         width: '100vw',
+//         height: '100vh',
+//       }}>
+//       <div style={{
+//         position: 'absolute',
+//         backgroundColor: 'red',
+//         borderRadius: '50%',
+//         transform: `translate(${position.x}px, ${position.y}px)`,
+//         left: -10,
+//         top: -10,
+//         width: 20,
+//         height: 20,
+//       }}/>
+//             <div />
+//         </div>
+//     );
+// }
+
+
+// copying object with the spread syntax
+
 import { useState } from "react";
 
-export default function MovingDot(){
-    const [position, setPosition] = useState({
-        x: 0,
-        y: 0,
+export default function Form(){
+    const [person, setPerson] = useState({
+        firstName: 'Daniel',
+        lastName: 'Datuhung',
+        email: 'DD@working.org'
     });
 
+    function handleFirstNameChange(e){
+        person.firstName = e.target.value;
+    }
+
+    function handleLastNameChange(e){
+        person.lastName = e.target.value;
+    }
+
+    function handleEmailChange(e){
+        person.email = e.target.value;
+    }
+
     return(
-        <div onPointerMove={e => {
-            setPosition({
-                x: e.clientX,
-                y: e.clientY,
-            });
-        }}
-        style={{
-        position: 'relative',
-        width: '100vw',
-        height: '100vh',
-      }}>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'red',
-        borderRadius: '50%',
-        transform: `translate(${position.x}px, ${position.y}px)`,
-        left: -10,
-        top: -10,
-        width: 20,
-        height: 20,
-      }} />
->
-            <div />
-        </div>
+        <>
+            <label>
+                First Name:
+                <input 
+                    value={person.firstName}
+                    onChange={handleFirstNameChange}
+                />
+            </label>
+            <label>
+                Last Name:
+                <input 
+                    value={person.lastName}
+                    onChange={handleLastNameChange}
+                />
+            </label>
+            <label>
+                Email:
+                <input 
+                    value={person.email}
+                    onChange={handleEmailChange}
+                />
+            </label>
+
+            <p>
+                {person.firstName}
+                {person.lastName}
+                ({person.email})
+            </p>
+        </>
     );
 }
