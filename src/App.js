b49f7aa9d4cc4b09e82d1601d7f8ad2182c8f8eb
selsadan <danielfrancis442@gmail.com>
2023-08-13 14:02:44 +0100

    Exercise on React object state

--- src/App.js
+++ src/App.js
@@ -3802,100 +3802,157 @@
 //     );
 // }
 
-import { useState } from "react";
+// import { useState } from "react";
 
-export default function FormO(){
-    const [person, setPerson] = useState({
-        name: 'Daniel Datuhung',
-        artwork: {
-            title: 'Computer Scientist',
-            city: 'Jos, Plateau State',
-            image: 'https://i.imgur.com/Sd1AgUOm.jpg'
-        }
+// export default function FormO(){
+//     const [person, setPerson] = useState({
+//         name: 'Daniel Datuhung',
+//         artwork: {
+//             title: 'Computer Scientist',
+//             city: 'Jos, Plateau State',
+//             image: 'https://i.imgur.com/Sd1AgUOm.jpg'
+//         }
+//     });
+
+//     function handleNameChange(e){
+//         setPerson({
+//             ...person,
+//             name: e.target.value
+//         });
+//     }
+
+//     function handleTitleChange(e){
+//         setPerson({
+//             ...person,
+//             artwork: {
+//                 ...person.artwork,
+//                 title : e.target.value
+//             }
+//         });
+//     }
+
+//     function handleCityChange(e){
+//         setPerson({
+//             ...person,
+//             artwork: {
+//                 ...person.artwork,
+//                 city : e.target.value
+//             }
+//         });
+//     }
+
+//     function handleImageChange(e){
+//         setPerson({
+//             ...person,
+//             artwork: {
+//                 ...person.artwork,
+//                 image : e.target.value
+//             }
+//         });
+//     }
+
+//     return(
+//         <>
+//             <label>
+//                 Name: 
+//                 <input 
+//                     value={person.name}
+//                     onChange={handleNameChange}
+//                 />
+//             </label>
+
+//             <label>
+//                 Title: 
+//                 <input 
+//                     value={person.artwork.title}
+//                     onChange={handleTitleChange}
+//                 />
+//             </label>
+
+//             <label>
+//                 City: 
+//                 <input 
+//                     value={person.artwork.city}
+//                     onChange={handleCityChange}
+//                 />
+//             </label>
+
+//             <label>
+//                 Image: 
+//                 <input 
+//                     value={person.artwork.image}
+//                     onChange={handleImageChange}
+//                 />
+//             </label>
+
+//             <p>
+//                 {person.artwork.title}
+//                 {' by '}
+//                 {person.name}
+//                 <br />
+//                 (located in {person.artwork.city})
+//             </p>
+//             <img 
+//                 src={person.artwork.image}
+//                 alt={person.artwork.title}
+//             />
+//         </>
+//     );
+// }
+
+import { useState } from 'react';
+
+export default function Scoreboard() {
+  const [player, setPlayer] = useState({
+    firstName: 'Ranjani',
+    lastName: 'Shettar',
+    score: 10,
+  });
+
+  function handlePlusClick() {
+    setPlayer({
+        ...player,
+        score: player.score +1
+    });;
+  }
+
+  function handleFirstNameChange(e) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value,
     });
+  }
 
-    function handleNameChange(e){
-        setPerson({
-            ...person,
-            name: e.target.value
-        });
-    }
+  function handleLastNameChange(e) {
+    setPlayer({
+        ...player,
+      lastName: e.target.value
+    });
+  }
 
-    function handleTitleChange(e){
-        setPerson({
-            ...person,
-            artwork: {
-                ...person.artwork,
-                title : e.target.value
-            }
-        });
-    }
-
-    function handleCityChange(e){
-        setPerson({
-            ...person,
-            artwork: {
-                ...person.artwork,
-                city : e.target.value
-            }
-        });
-    }
-
-    function handleImageChange(e){
-        setPerson({
-            ...person,
-            artwork: {
-                ...person.artwork,
-                image : e.target.value
-            }
-        });
-    }
-
-    return(
-        <>
-            <label>
-                Name: 
-                <input 
-                    value={person.name}
-                    onChange={handleNameChange}
-                />
-            </label>
-
-            <label>
-                Title: 
-                <input 
-                    value={person.artwork.title}
-                    onChange={handleTitleChange}
-                />
-            </label>
-
-            <label>
-                City: 
-                <input 
-                    value={person.artwork.city}
-                    onChange={handleCityChange}
-                />
-            </label>
-
-            <label>
-                Image: 
-                <input 
-                    value={person.artwork.image}
-                    onChange={handleImageChange}
-                />
-            </label>
-
-            <p>
-                {person.artwork.title}
-                {' by '}
-                {person.name}
-                <br />
-                (located in {person.artwork.city})
-            </p>
-            <img 
-                src={person.artwork.image}
-                alt={person.artwork.title}
-            />
-        </>
-    );
+  return (
+    <>
+      <label>
+        Score: <b>{player.score}</b>
+        {' '}
+        <button onClick={handlePlusClick}>
+          +1
+        </button>
+      </label>
+      <label>
+        First name:
+        <input
+          value={player.firstName}
+          onChange={handleFirstNameChange}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          value={player.lastName}
+          onChange={handleLastNameChange}
+        />
+      </label>
+    </>
+  );
 }
