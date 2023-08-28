e50da22553dc4a88628051a2d6e54f6abecfec08
selsadan <danielfrancis442@gmail.com>
2023-08-28 13:26:05 +0100

    I keep working on React

--- src/App.js
+++ src/App.js
@@ -4146,17 +4146,47 @@
 //     </>
 // );
 // }
+// import { useState } from "react";
+
+// let nextId = 0;
+
+// export default function List (){
+//     const [name, setName] = useState('');
+//     const [artist, setArtist] = useState ([]);
+
+//     return(
+//         <>
+//             <h1>Inspiring Sculptors:</h1>
+//             <input 
+//                 value={name}
+//                 onChange={e => setName(e.target.value)}
+//             />
+//             <button onClick={() => {
+//                 setArtist([
+//                     ...artist,
+//                     {id: nextId++, name: name}
+//                 ]);
+//             }}>Add</button>
+//             <ul>
+//                 {artist.map(artist => (
+//                     <li key={artist.id}>{artist.name}</li>
+//                 ))}
+//             </ul>
+//         </>
+//     );
+// }
+
 import { useState } from "react";
 
 let nextId = 0;
 
-export default function List (){
+export default function List(){
     const [name, setName] = useState('');
-    const [artist, setArtist] = useState ([]);
+    const [artist, setArtist] = useState([]);
 
     return(
         <>
-            <h1>Inspiring Sculptors:</h1>
+            <h1>Inspiring Sculptors is Good!</h1>
             <input 
                 value={name}
                 onChange={e => setName(e.target.value)}
