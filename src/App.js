6ebaf2b51ad310e0b25aa7bb9cd7526a57a117eb
selsadan <danielfrancis442@gmail.com>
2023-08-28 10:25:04 +0100

    working on React state using array

--- src/App.js
+++ src/App.js
@@ -4087,32 +4087,62 @@
 //     );
 // }
 
+// import { useState } from "react";
+
+// let nextId = 0;
+
+// export default function App(){
+//     const [name, setName] = useState('');
+//     const [artists, setArtists] = useState([]);
+
+//     return(
+//         <>
+//             <h1>Inspiring Sculptures:</h1>
+//             <input 
+//                 value={name}
+//                 onChange={e => setName(e.target.value)}
+//             />
+//             <button onClick={() =>{
+//                 setArtists([
+//                     ...artists,
+//                     {id: nextId++, name: name}
+//                 ]);
+//             }}>Add</button>
+//             <ul>
+//                 {artists.map(artist =>(
+//                     <li key={artist.id}>{artist.name}</li>
+//                 ))}
+//             </ul>
+//         </>
+//     );
+// }
+
 import { useState } from "react";
 
 let nextId = 0;
 
-export default function App(){
-    const [name, setName] = useState('');
-    const [artists, setArtists] = useState([]);
+export default function List(){
+    const [name, setName] = useState ('');
+    const [artist, setArtists] = useState([]);
 
-    return(
-        <>
-            <h1>Inspiring Sculptures:</h1>
-            <input 
-                value={name}
-                onChange={e => setName(e.target.value)}
-            />
-            <button onClick={() =>{
-                setArtists([
-                    ...artists,
-                    {id: nextId++, name: name}
-                ]);
-            }}>Add</button>
-            <ul>
-                {artists.map(artist =>(
-                    <li key={artist.id}>{artist.name}</li>
-                ))}
-            </ul>
-        </>
-    );
+return(
+    <>
+        <h1>Inspiring Sculpture:</h1>
+        <input 
+            value={name}
+            onChange={e => setName(e.target.value)}
+        />
+        <button onClick={() => {
+            artist.push({
+                id: nextId++,
+                name: name,
+            });
+        }}>Add</button>
+        <ul>
+            {artist.map(artist => {
+                <li key={artist.id}>{artist.name}</li>
+            })}
+        </ul>
+    </>
+);
 }
